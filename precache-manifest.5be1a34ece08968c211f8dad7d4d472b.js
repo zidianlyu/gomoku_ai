e6d4097588e91b4c69250b13f5267a8301e86172
Zidian Lyu <zlu@ucdavis.edu>
2019-10-31 18:23:36 -0700

    upload to git

---
+++ precache-manifest.5be1a34ece08968c211f8dad7d4d472b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4478d8d76123f7708806ac508d3d6bc",
+    "url": "/gomoku_ai/index.html"
+  },
+  {
+    "revision": "fb973e2bd0043f451f1f",
+    "url": "/gomoku_ai/static/js/2.eb8e77ed.chunk.js"
+  },
+  {
+    "revision": "4b21cf54f7491fdaa762",
+    "url": "/gomoku_ai/static/js/main.885668be.chunk.js"
+  },
+  {
+    "revision": "72a772a5dc526f045d69",
+    "url": "/gomoku_ai/static/js/runtime-main.89fc8e6c.js"
+  },
+  {
+    "revision": "42ec96183d4742cb07e48d644818edfa",
+    "url": "/gomoku_ai/static/media/board.42ec9618.png"
+  }
+]);
